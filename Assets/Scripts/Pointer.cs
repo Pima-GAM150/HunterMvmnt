2a5a18b87bf0461918512569d43032baf22c7b49
Hunter <hbova@mail.pima.edu>
2019-05-14 16:36:50 -0700

    Initial push

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Pointer : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit currentHit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out currentHit, Mathf.Infinity, LayerMask.GetMask("Default")))
+        {
+            if (currentHit.collider.gameObject.tag == "Win") Debug.Log("Win");
+            //if (gameObject.tag == "Win") SceneManager.LoadScene(LevelLoader.find.levelIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/rotateSphere.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateSphere : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(new Vector3(Time.deltaTime * 5, 0f,0f));
+        if (Input.GetKey(KeyCode.RightArrow)) transform.Rotate(new Vector3(Time.deltaTime * -5, 0f, 0f));
+        if (Input.GetKey(KeyCode.UpArrow)) transform.Rotate(new Vector3(0f, 0f, Time.deltaTime * -5));
+        if (Input.GetKey(KeyCode.DownArrow)) transform.Rotate(new Vector3(0f, 0f, Time.deltaTime * 5));
+    }
+}
